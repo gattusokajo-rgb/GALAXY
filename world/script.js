e77b4abc509daa10e7b65b38fb81a8ec4da2686e
Kajo <gattusokajo@gmail.com>
2026-05-02 16:18:55 +0800

    cantik

--- world/script.js
+++ world/script.js
@@ -1,1 +1,17 @@
-console.log("Website loaded!");
+const starsContainer = document.querySelector('.stars');
+
+for (let i = 0; i < 50; i++) {
+  const star = document.createElement('div');
+  star.classList.add('dot');
+
+  // random posisi
+  star.style.top = Math.random() * 100 + "%";
+  star.style.left = Math.random() * 100 + "%";
+
+  // beberapa jadi merah
+  if (Math.random() < 0.05) {
+    star.classList.add('red');
+  }
+
+  starsContainer.appendChild(star);
+}
